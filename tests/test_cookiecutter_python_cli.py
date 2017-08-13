e4a6d34f4528d65f257120f8edb4dc864099e3a6
Matt Menzenski <matt@mobilgov.com>
2017-08-13 17:33:34 -0500

    proper path naming in test

--- tests/test_cookiecutter_python_cli.py
+++ tests/test_cookiecutter_python_cli.py
@@ -4,14 +4,15 @@ import os.path
 import subprocess
 
 
-def test_cookiecuttering(tempdir):
+def test_cookiecuttering(tmpdir):
     root_dir = os.path.join(os.path.dirname(__file__), os.pardir)
     with open(os.path.join(root_dir, 'cookiecutter.json')) as config_file:
         config_data = json.load(config_file)
         package_name = config_data['package_name']
     assert package_name and isinstance(package_name, str)
 
-    output_dir = tempdir.mkdir(package_name)
+    output_dir = tmpdir.mkdir(package_name)
+    output_dir_name = output_dir.dirname
     subprocess.check_call([
         'cookiecutter',
         'https://github.com/menzenski/cookiecutter-python-cli',
@@ -20,19 +21,19 @@ def test_cookiecuttering(tempdir):
         '--no-input',
         '--overwrite-if-exists',
         '--output-dir',
-        output_dir,
+        output_dir_name,
     ])
 
     subprocess.check_call([
         'make',
         '-C',
-        output_dir,
+        output_dir_name,
         'setup',
     ])
 
     subprocess.check_call([
         'make',
         '-C',
-        output_dir,
+        output_dir_name,
         'test',
     ])
