eebac6ec61467b05987d24955d61d2c83abb320a
Matt Menzenski <matt@mobilgov.com>
2017-08-13 22:19:13 -0500

    call make from correct dir

--- tests/test_cookiecutter_python_cli.py
+++ tests/test_cookiecutter_python_cli.py
@@ -6,13 +6,15 @@ import subprocess
 
 def test_cookiecuttering(tmpdir):
     root_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+
     with open(os.path.join(root_dir, 'cookiecutter.json')) as config_file:
         config_data = json.load(config_file)
+        repo_name = config_data['repo_name']
         package_name = config_data['package_name']
-
     assert package_name and isinstance(package_name, str)
 
-    output_dir = tmpdir.mkdir(package_name)
+    output_dir = tmpdir.mkdir()
+
     subprocess.check_call([
         'cookiecutter',
         'https://github.com/menzenski/cookiecutter-python-cli',
@@ -26,10 +28,12 @@ def test_cookiecuttering(tmpdir):
         # str(output_dir),
     ])
 
+    repo_dir = output_dir.join(repo_name)
+
     subprocess.check_call([
         'make',
         '-C',
-        output_dir.strpath,
+        repo_dir.strpath,
         # str(output_dir),
         'setup',
     ])
@@ -37,7 +41,7 @@ def test_cookiecuttering(tmpdir):
     subprocess.check_call([
         'make',
         '-C',
-        output_dir.strpath,
+        repo_dir.strpath,
         # str(output_dir),
         'test',
     ])
